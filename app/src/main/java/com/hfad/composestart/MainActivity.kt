0d9b32a8fda396044a6eafe79358e9ae2de465fd
RekaEva <tanechkarek@rambler.ru>
2023-08-02 18:04:29 +0300

    тtable

---
+++ app/src/main/java/com/hfad/composestart/MainActivity.kt
@@ -0,0 +1,62 @@
+package com.hfad.composestart
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.hfad.composestart.ui.theme.ComposeStartTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+            }
+        }
+}
+
+@Preview
+@Composable
+fun TimesTable()
+{
+    Column( modifier = Modifier.fillMaxSize()) {
+        for (i in 1 until 10)
+        {
+            Row(
+                modifier = Modifier.fillMaxWidth().weight(1f)
+            ){
+                for (j in 1 until 10)
+                {
+                    val color = if ((i + j) % 2 == 0){
+                        Color.Yellow
+                    }
+                    else {
+                        Color.White
+                    }
+                    Box(
+                        modifier = Modifier.fillMaxHeight()
+                            .weight(1f).border(width = 1.dp, color = Color.DarkGray)
+                            .background(color = color),
+                    contentAlignment =  Alignment.Center
+                    ){
+                        Text(text = "${i *j}", fontSize = 20.sp)
+                    }
+
+                }
+            }
+        }
+    }
+
+}
