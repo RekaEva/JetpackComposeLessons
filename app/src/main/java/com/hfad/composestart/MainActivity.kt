5e6b40ace2fbe97234d0b4879fe29fe983ac52e0
RekaEva <tanechkarek@rambler.ru>
2023-08-03 13:46:45 +0300

    before add DarkThem

--- app/src/main/java/com/hfad/composestart/MainActivity.kt
+++ app/src/main/java/com/hfad/composestart/MainActivity.kt
@@ -3,60 +3,47 @@ package com.hfad.composestart
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.hfad.composestart.ui.theme.ComposeStartTheme
+import com.hfad.composestart.ui.theme.InstagramProfileCard
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
+            ComposeStartTheme{
+                InstagramProfileCard()
+            }
             }
         }
 }
 
 @Preview
 @Composable
-fun TimesTable()
+fun CardTest()
 {
-    Column( modifier = Modifier.fillMaxSize()) {
-        for (i in 1 until 10)
-        {
-            Row(
-                modifier = Modifier.fillMaxWidth().weight(1f)
-            ){
-                for (j in 1 until 10)
-                {
-                    val color = if ((i + j) % 2 == 0){
-                        Color.Yellow
-                    }
-                    else {
-                        Color.White
-                    }
-                    Box(
-                        modifier = Modifier.fillMaxHeight()
-                            .weight(1f).border(width = 1.dp, color = Color.DarkGray)
-                            .background(color = color),
-                    contentAlignment =  Alignment.Center
-                    ){
-                        Text(text = "${i *j}", fontSize = 20.sp)
-                    }
-
-                }
-            }
-        }
+    Card(
+        shape = RoundedCornerShape(topStart = 4.dp, topEnd = 4.dp),
+        backgroundColor = Color.Green,
+        contentColor = Color.DarkGray,
+        border = BorderStroke(1.dp, Color.Black)
+    ) {
+        Text(text = "Hello World!",
+        modifier = Modifier.padding(8.dp))
     }
-
 }
